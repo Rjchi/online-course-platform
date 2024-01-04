175da9f0a705336cd28b2c2071570e4c89fac27b
Rjchi <richardospina18@gmail.com>
2024-01-03 19:09:58 -0500

    [Feat] Agregamos el registro, actualización y gestión del avatar para ese usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,7 @@
+import fs from "fs";
+import path from "path";
 import bcrypt from "bcryptjs";
+
 import models from "../models";
 import token from "../service/token";
 
@@ -103,6 +106,103 @@ export default {
       console.log(error.message);
       return res.status(500).send({
         msg: "HUBO UN ERROR",
+      });
+    }
+  },
+  register_admin: async (req, res) => {
+    try {
+      const VALID_USER = await models.User.findOne({ email: req.body.email });
+
+      if (VALID_USER)
+        return res
+          .status(200)
+          .json({ message: 403, msg: "EL USUARIO INGRESADO YA EXISTE" });
+
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+
+      if (req.files && req.files.avatar) {
+        /**----------------------------------------------------------
+         * | Tomamos el nombre de la ruta de donde se almacena
+         * | el avatar
+         * ----------------------------------------------------------*/
+        const img_path = req.files.avatar.path;
+        const avatar_name = img_path.split("\\")[2];
+
+        req.body.avatar = avatar_name;
+      }
+
+      const User = await models.User.create(req.body);
+      res.status(200).json({ user: User });
+    } catch (error) {
+      console.log(error.messge);
+      return res.status(500).send({
+        msg: "OCURRIO UN PROBLEMA",
+      });
+    }
+  },
+  getImage: async (req, res) => {
+    try {
+      const img = req.params['img'];
+
+      if (!img) return res.status(500).json({ msg: "OCURRIO UN PROBLEMA" });
+
+      /**----------------------------------------
+       * | Valicamos si el archivo existe o no
+       * | con stat de fs
+       * ----------------------------------------*/
+      fs.stat('./uploads/user/' + img, function(err) {
+        if (!err) {
+          let path_img = './uploads/user/' + img;
+
+          /**------------------------------------------------------------------
+           * | Asi visualizamos desde la api la imagen que estamos almacenando
+           * ------------------------------------------------------------------*/
+          return res.status(200).sendFile(path.resolve(path_img));
+        } else {
+          /**----------------------------------------------------------
+           * | En caso de error retornamos una imagen por defecto
+           * ----------------------------------------------------------*/
+          let path_img = './uploads/default.png';
+
+          return res.status(200).sendFile(path.resolve(path_img));
+        }
+      });
+    } catch (error) {
+      console.log(error.messge);
+      return res.status(500).send({
+        msg: "OCURRIO UN PROBLEMA",
+      });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const VALID_USER = await models.User.findOne({ email: req.body.email, _id: { $ne: req.body._id } });
+
+      if (VALID_USER)
+        return res
+          .status(200)
+          .json({ message: 403, msg: "EL USUARIO INGRESADO YA EXISTE" });
+
+      if (req.body.password) {
+        req.body.password = await bcrypt.hash(req.body.password, 10);
+      }
+
+      if (req.files && req.files.avatar) {
+        const img_path = req.files.avatar.path;
+        const avatar_name = img_path.split("\\")[2];
+
+        req.body.avatar = avatar_name;
+      }
+
+      /**-----------------------------------------------------------
+       * | Ubicamos el usuario en base al id y luego lo editamos
+       * -----------------------------------------------------------*/
+      const User = await models.User.findByIdAndUpdate({ id: req.body._id }, req.body);
+      res.status(200).json({ user: User });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).send({
+        msg: "OCURRIO UN PROBLEMA",
       });
     }
   },
--- router/User.js
+++ router/User.js
@@ -1,11 +1,21 @@
+import multiparty from "connect-multiparty"; // (Middleware) Esta libreria para recepcionar la img, procesarla y almacenarla
 import routerx from "express-promise-router";
 import userController from "../controllers/UserController";
+
+/**---------------------------------
+ * | parametros: DONDE ALMACENAR
+ * ---------------------------------*/
+var path = multiparty({uploadDir: './uploads/user'})
 
 const router = routerx();
 
 router
   .post("/register", userController.register)
   .post("/login", userController.login)
-  .post("/login_admin", userController.login_admin);
+  .post("/login_admin", userController.login_admin)
+
+  .post("/update", path, userController.update)
+  .post("/register_admin", path, userController.register_admin)
+  .get("/imagen-usuario/:img", userController.getImage);
 
 export default router;
