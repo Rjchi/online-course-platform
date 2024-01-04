194e334c1efc60d8de24cd711db05fb2f7f1eb30
Rjchi <richardospina18@gmail.com>
2024-01-03 20:01:39 -0500

    [Feat] Agregar enpoint de actualización y eliminación de un usuario

--- router/User.js
+++ router/User.js
@@ -5,17 +5,19 @@ import userController from "../controllers/UserController";
 /**---------------------------------
  * | parametros: DONDE ALMACENAR
  * ---------------------------------*/
-var path = multiparty({uploadDir: './uploads/user'})
+var path = multiparty({ uploadDir: "./uploads/user" });
 
 const router = routerx();
 
 router
-  .post("/register", userController.register)
   .post("/login", userController.login)
+  .post("/register", userController.register)
   .post("/login_admin", userController.login_admin)
 
-  .post("/update", path, userController.update)
-  .post("/register_admin", path, userController.register_admin)
-  .get("/imagen-usuario/:img", userController.getImage);
+  .get("/list", userController.list)
+  .put("/update", path, userController.update)
+  .delete("/delete/:id", userController.remove)
+  .get("/imagen-usuario/:img", userController.getImage)
+  .post("/register_admin", path, userController.register_admin);
 
 export default router;
