5cb5f3714102f12f783d85d7736b48f4ae501f53
Rjchi <richardospina18@gmail.com>
2024-01-04 21:59:33 -0500

    [Refactor] Modificar el resource de usuario en la propiedad avatar

--- resource/user/User.js
+++ resource/user/User.js
@@ -5,8 +5,9 @@ export default {
       rol: user.rol,
       name: user.name,
       email: user.email,
-      avatar:
-        process.env.URL_BACKEND + "/api/auth/imagen-usuario/" + user.avatar,
+      avatar: user.avatar
+        ? process.env.URL_BACKEND + "/api/auth/imagen-usuario/" + user.avatar
+        : null,
       surname: user.surname,
       profession: user.profession,
       description: user.description,
