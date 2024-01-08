658e3fed9ae0c29e169b0997b921f8cbf75a21f1
Rjchi <richardospina18@gmail.com>
2024-01-08 17:31:16 -0500

    [Feat] Registrar router de categoria en el archivo index

--- router/index.js
+++ router/index.js
@@ -1,8 +1,10 @@
 import routerx from "express-promise-router";
+
 import User from "./User";
+import Categorie from "./Categorie";
 
 const router = routerx();
 
-router.use("/auth", User);
+router.use("/auth", User).use("/categories", Categorie);
 
 export default router;
