9fdf826c50f737589ba7a618add7e69817917d73
Rjchi <richardospina18@gmail.com>
2024-01-10 20:16:40 -0500

    [Fix] Corregir rutas y agregar importaciones faltantes

--- controllers/CategorieController.js
+++ controllers/CategorieController.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 import models from "../models";
 import resource from "../resource";
 
@@ -116,9 +119,9 @@ export default {
 
       if (!img) return res.status(500).json({ msg: "OCURRIO UN PROBLEMA" });
 
-      fs.stat("./uploads/categorie" + img, function (err) {
+      fs.stat("./uploads/categorie/" + img, function (err) {
         if (!err) {
-          let path_img = "./uploads/categorie" + img;
+          let path_img = "./uploads/categorie/" + img;
 
           return res.status(200).sendFile(path.resolve(path_img));
         } else {
