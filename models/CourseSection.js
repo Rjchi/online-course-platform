0e2043841766aa4589258d7bc794380ac695c5f3
Rjchi <richardospina18@gmail.com>
2024-04-03 14:32:03 -0500

    [Feat] Agregar modelo para las secciones de un curso

---
+++ models/CourseSection.js
@@ -0,0 +1,14 @@
+import { Schema } from "mongoose";
+
+const CourseSectionSchema = new Schema(
+  {
+    title: { type: String, maxlength: 250, required: true },
+    state: { type: Number, maxlength: 1, default: 1 }, // 1 es activo y 2 es inactivo
+    course: { type: Schema.ObjectId, ref: "course", required: true },
+  },
+  { timestamps: true }
+);
+
+const CourseSection = mongoose.model("course_section", CourseSectionSchema);
+
+export default CourseSection;
