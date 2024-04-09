7d01bb53605929d2ad930f65ad624be8145f5323
Rjchi <richardospina18@gmail.com>
2024-04-09 09:32:07 -0500

    [Feat] Agregar nuevo modelo para una clase de una sección

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 import User from "./User";
 import Course from "./Course";
 import Categorie from "./Categorie";
+import CourseClass from "./CourseClass";
 import CourseSection from "./CourseSection";
 
 export default {
   User,
   Course,
   Categorie,
+  CourseClass,
   CourseSection,
 };
