26cdd7f4463b55943ddf935a16f3fd14f1e2c1c5
Rjchi <richardospina18@gmail.com>
2024-04-09 09:54:24 -0500

    [Feat] Agregar nuevos enpoints para las clases de un curso

--- router/index.js
+++ router/index.js
@@ -3,14 +3,16 @@ import routerx from "express-promise-router";
 import User from "./User";
 import Course from "./Course";
 import Categorie from "./Categorie";
+import CourseClass from "./CourseClass";
 import CourseSection from "./CourseSection";
 
 const router = routerx();
 
 router
   .use("/auth", User)
-  .use("/categories", Categorie)
   .use("/courses", Course)
+  .use("/categories", Categorie)
+  .use("/course-class", CourseClass)
   .use("/course-section", CourseSection);
 
 export default router;
