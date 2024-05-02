c69acc28632c3226b78815eb527f776be490e947
Rjchi <richardospina18@gmail.com>
2024-05-02 16:04:44 -0500

    [Feat] Agregar a la resouesta los cursos que pertenezcan a la campaña de descuento de tipo banner

--- controllers/store/HomeController.js
+++ controllers/store/HomeController.js
@@ -4,70 +4,11 @@ import models from "../../models";
 import apiResource from "../../resource";
 
 export default {
-  register: async (req, res) => {
-    try {
-      const VALID_SECTION = await models.CourseSection.findOne({
-        title: req.body.title,
-        course: req.body.course,
-      });
-
-      if (VALID_SECTION) {
-        return res.status(200).json({
-          msg: 403,
-          message_text: "LA SECCIÓN YA EXISTE",
-        });
-      }
-
-      const newSection = await models.CourseSection.create(req.body);
-
-      return res.status(200).json({
-        section: newSection,
-      });
-    } catch (error) {
-      console.log(error.message);
-      return res.status(500).json({
-        msg: "HUBO UN ERROR",
-      });
-    }
-  },
-  update: async (req, res) => {
-    try {
-      const VALID_SECTION = await models.CourseSection.findOne({
-        title: req.body.title,
-        course: req.body.course,
-        _id: { $ne: req.body._id },
-      });
-
-      if (VALID_SECTION) {
-        return res.status(200).json({
-          msg: 403,
-          message_text: "LA SECCIÓN YA EXISTE",
-        });
-      }
-
-      const EditCourseSection = await models.CourseSection.findByIdAndUpdate(
-        { _id: req.body._id },
-        req.body
-      );
-
-      const NEditCourseSection = await models.CourseSection.findById({
-        _id: EditCourseSection._id,
-      });
-
-      return res.status(200).json({
-        section: NEditCourseSection,
-      });
-    } catch (error) {
-      console.log(error.message);
-      return res.status(500).json({
-        msg: "HUBO UN ERROR",
-      });
-    }
-  },
   list: async (req, res) => {
     try {
       let categories = await models.Categorie.find({ state: 1 });
       let CATEGORIES_LIST = [];
+      let time_now = req.query.time_now;
 
       for (let categorie of categories) {
         let count_courses = await models.Course.countDocuments({
@@ -150,10 +91,30 @@ export default {
         });
       }
 
+      let campaing_baner = await models.Discount.findOne({
+        type_campaing: 3,
+        start_date_num: { $lte: time_now },
+        end_date_num: { $gte: time_now },
+      });
+
+      let COURSES_BANNER = [];
+
+      if (campaing_baner) {
+        for (const course of campaing_baner.courses) {
+          let MODEL_COURSES = await models.Course.findById({ _id: course });
+
+          COURSES_BANNER.push(
+            apiResource.Course.apiResourceCourse(MODEL_COURSES)
+          );
+        }
+      }
+
       return res.status(200).json({
         categories: CATEGORIES_LIST,
         courses_top: COURSES_TOPS,
         courses_sections: CATEGORIES_SECTIONS,
+        courses_banner: COURSES_BANNER,
+        campaing_banner: campaing_baner,
       });
     } catch (error) {
       console.log(error);
@@ -193,20 +154,6 @@ export default {
       console.log(error.messge);
       return res.status(500).send({
         msg: "OCURRIO UN PROBLEMA",
-      });
-    }
-  },
-  remove: async (req, res) => {
-    try {
-      await models.CourseSection.findByIdAndDelete({ _id: req.params["id"] });
-
-      return res.status(200).json({
-        msg: "LA SECCIÓN SE ELIMINO CORRECTAMENTE",
-      });
-    } catch (error) {
-      console.log(error.message);
-      return res.status(500).json({
-        msg: "HUBO UN ERROR",
       });
     }
   },
