a39dcf8351fe4f5d6a158fe59031c1fc186433e5
Rjchi <richardospina18@gmail.com>
2024-05-02 16:03:11 -0500

    Eliminar enpoints inecesarios

--- router/store/Home.js
+++ router/store/Home.js
@@ -5,9 +5,6 @@ const router = routerx();
 
 router
   .get("/list", homeController.list)
-  .put("/update", homeController.update)
-  .post("/register", homeController.register)
-  .delete("/remove/:id", homeController.remove)
   .get("/imagen-usuario/:img", homeController.getImage)
 
 export default router;
