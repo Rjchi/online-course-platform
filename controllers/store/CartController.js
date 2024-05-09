1b9c5380f1b16ab2cc6eaf733b1853079ce686ef
Rjchi <richardospina18@gmail.com>
2024-05-09 15:17:16 -0500

    [Feat] Agregar controlador del carrito de compras

---
+++ controllers/store/CartController.js
@@ -0,0 +1,42 @@
+export default {
+  list: async (req, res) => {
+    try {
+        
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "OCURRIO UN ERROR",
+      });
+    }
+  },
+  update: async (req, res) => {
+    try {
+        
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "OCURRIO UN ERROR",
+      });
+    }
+  },
+  remove: async (req, res) => {
+    try {
+        
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "OCURRIO UN ERROR",
+      });
+    }
+  },
+  register: async (req, res) => {
+    try {
+        
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "OCURRIO UN ERROR",
+      });
+    }
+  },
+};
