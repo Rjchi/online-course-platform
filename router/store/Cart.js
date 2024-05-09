963864e3c2deb2c4379a1d4a69af763f81b08e14
Rjchi <richardospina18@gmail.com>
2024-05-09 15:15:37 -0500

    [Feat] Agregar enpoints para el carrito de compras

--- router/store/Cart.js
+++ router/store/Cart.js
@@ -7,8 +7,8 @@ const router = routerx();
 
 router
   .get("/list", [auth.verifyTienda], CartController.list)
-  .put("/update", [auth.verifyTienda, path], CartController.update)
-  .post("/register", [auth.verifyTienda, path], CartController.register)
+  .put("/update", [auth.verifyTienda], CartController.update)
+  .post("/register", [auth.verifyTienda], CartController.register)
   .delete("/remove/:id", [auth.verifyTienda], CartController.remove);
 
 export default router;
