487da4a519d4d4973fb477c7bdae474fd2be0768
Rjchi <richardospina18@gmail.com>
2024-05-14 15:53:15 -0500

    [Feat] Registrar api resource de cart

--- resource/index.js
+++ resource/index.js
@@ -1,9 +1,11 @@
 import Course from "./Course";
 import User from "./user/User";
+import Cart from "./store/Cart";
 import Categorie from "./Categorie";
 
 export default {
   User,
+  Cart,
   Course,
   Categorie,
 };
