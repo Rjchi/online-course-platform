72149b7c388e3855c79fbc15e204a4b22315f30e
Rjchi <richardospina18@gmail.com>
2024-05-17 10:29:58 -0500

    [Feat] Registrar modelos para el detallado de una compra

--- models/index.js
+++ models/index.js
@@ -2,20 +2,26 @@ import User from "./User";
 import Cupone from "./Cupone";
 import Course from "./Course";
 import Cart from "./store/Cart";
+import Sale from "./store/Sale";
 import Discount from "./Discount";
 import Categorie from "./Categorie";
 import CourseClass from "./CourseClass";
 import CourseSection from "./CourseSection";
+import SaleDetail from "./store/SaleDetail";
 import CourseClassFile from "./CourseClassFile";
+import CourseStudent from "./store/CourseStudent";
 
 export default {
   User,
   Cart,
+  Sale,
   Course,
   Cupone,
   Discount,
   Categorie,
+  SaleDetail,
   CourseClass,
+  CourseStudent,
   CourseSection,
   CourseClassFile,
 };
