f2e4e51e7b61d7d739263401fe137f106cb5fcbb
Rjchi <richardospina18@gmail.com>
2024-05-17 13:20:09 -0500

    [Feat] Agregar metodo para el envio de un email

--- controllers/store/SaleController.js
+++ controllers/store/SaleController.js
@@ -1,7 +1,63 @@
 import models from "../../models";
 import token from "../../service/token";
 
+import fs from "fs";
+import ejs from "ejs";
+import handlebars from "handlebars";
+import nodemailer from "nodemailer";
+import smtpTransport from "nodemailer-smtp-transport";
+
 export default {
+  send_mail: async (req, res) => {
+    try {
+      var readHTMLFile = function (path, callback) {
+        fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+          if (err) {
+            throw err;
+            callback(err);
+          } else {
+            callback(null, html);
+          }
+        });
+      };
+
+      var transporter = nodemailer.createTransport(
+        smtpTransport({
+          service: "gmail",
+          host: "smtp.gmail.com",
+          auth: {
+            user: `${process.env.EMAIL}`,
+            pass: `${process.env.EMAIL_PASSWORD}`,
+          },
+        })
+      );
+
+      readHTMLFile(process.cwd() + "/mails/email_sale.html", (err, html) => {
+        let rest_html = ejs.render(html, {});
+
+        var template = handlebars.compile(rest_html);
+        var htmlToSend = template({ op: true });
+
+        var mailOptions = {
+          from: `${process.env.EMAIL}`,
+          to: email_cliente,
+          subject: "Finaliza tu compra " + orden._id,
+          html: htmlToSend,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (!error) {
+            console.log("Email sent: " + info.response);
+          }
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "OCURRIO UN ERROR",
+      });
+    }
+  },
   register: async (req, res) => {
     try {
       let user = await token.decode(req.headers.token);
