5f10a0c2186103f45765ca5934ae0c16e3071289
Rjchi <richardospina18@gmail.com>
2024-05-24 11:32:14 -0500

    [Feat] Agregar modelo de course student

---
+++ models/client/CourseStudent.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const CourseStudentSchema = new Schema(
+  {
+    user:           { type: Schema.ObjectId, ref: "user", required: true },
+    course:         { type: Schema.ObjectId, ref: "course", required: true },
+    clases_checked: [{ type: String }],
+    state:          { type: Number, default: 1 }, // 1 significa curso iniciado 2 terminado
+  },
+  { timestamps: true }
+);
+
+const CourseStudent = mongoose.model("course_student", CourseStudentSchema);
+
+export default CourseStudent;
