ec500cc824e5a67870ec3ed54c8b6704a148a2f4
Rjchi <richardospina18@gmail.com>
2024-05-27 15:09:55 -0500

    [Feat] Agregar consulta para los cursos activos

--- controllers/client/ProfileStudentController.js
+++ controllers/client/ProfileStudentController.js
@@ -1,5 +1,21 @@
-import { decode } from "../../service/token";
 import models from "../../models";
+import apiResource from "../../resource";
+import { decode } from "../../service/token";
+
+const numeroDeClases = async (course) => {
+  let n_clases = 0;
+  let sections = await models.CourseSection.find({ course: course._id });
+
+  for (let section of sections) {
+    let clases = await models.CourseClass.countDocuments({
+      section: section._id,
+    });
+
+    n_clases += clases;
+  }
+
+  return n_clases;
+};
 
 export default {
   profileStudent: async (req, res) => {
@@ -31,28 +47,54 @@ export default {
       let termined_course_news = [];
       let enrolled_course_news = [];
 
+      /**--------------------
+       * | Enrolled course
+       * --------------------*/
       let enrolled_course = await models.CourseStudent.find({
         user: user._id,
-        state: 2,
       });
 
       for (let e_course of enrolled_course) {
         let course = await models.Course.findOne({ _id: e_course.course });
+        let Nclases = await numeroDeClases(course);
 
         enrolled_course_news.push({
-
-        })
+          percentage: (
+            (e_course.clases_checked.length / Nclases) *
+            100
+          ).toFixed(2), // redondeo de dos decimales
+          clases_checked: e_course.clases_checked,
+          course: apiResource.Course.apiResourceCourse(course, null, Nclases),
+        });
       }
-      // let actived_course = await models.CourseStudent.find({
-      //   user: user._id,
-      // });
-      //   let termined_course = await models.CourseStudent.find({
-      //     user: user._id,
-      //     clases_checked: {
-      //       $ne: [], // diferente a vacio
-      //     },
-      //     state: 1,
-      //   });
+
+      /**------------------
+       * | Actived Course
+       * ------------------*/
+
+      let actived_courses = await models.CourseStudent.find({
+        user: user._id,
+        clases_checked: {
+          $ne: [], // diferente a vacio
+        },
+        state: 1,
+      });
+
+      for (let a_course of actived_courses) {
+        let course = await models.Course.findOne({
+          _id: a_course.course,
+        });
+        let Nclases = await numeroDeClases(course);
+
+        actived_course_news.push({
+          percentage: (
+            (a_course.clases_checked.length / Nclases) *
+            100
+          ).toFixed(2), // redondeo de dos decimales
+          clases_checked: a_course.clases_checked,
+          course: apiResource.Course.apiResourceCourse(course, null, Nclases),
+        });
+      }
 
       return (
         res.status(200),
@@ -76,7 +118,7 @@ export default {
           },
           actived_course_news: [],
           termined_course_news: [],
-          enrolled_course_news: [],
+          enrolled_course_news: enrolled_course_news,
         })
       );
     } catch (error) {
