527c5965c15fc67d9b3784016142b782d26bf523
Rjchi <richardospina18@gmail.com>
2024-05-28 13:46:48 -0500

    [Refactor] Agregar el multiparty para el cambio del avatar del usuario

--- router/client/ProfileStudent.js
+++ router/client/ProfileStudent.js
@@ -1,12 +1,15 @@
+import multiparty from "connect-multiparty";
 import routerx from "express-promise-router";
 
 import auth from "../../service/auth";
 import profileStudentController from "../../controllers/client/ProfileStudentController";
 
+var path = multiparty({ uploadDir: "./uploads/user" });
+
 const router = routerx();
 
 router
   .get("/client", [auth.verifyTienda], profileStudentController.profileStudent)
-  .put("/update", [auth.verifyTienda], profileStudentController.updateStudent);
+  .put("/update", [auth.verifyTienda, path], profileStudentController.updateStudent);
 
 export default router;
